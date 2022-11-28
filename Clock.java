aba9d3b66becfe78324078738b3e183da5447e50
ritaaktay <ritaaktay@gmail.com>
2022-11-28 07:12:26 +0000

    ANSI color codes

--- Clock.java
+++ Clock.java
@@ -2,11 +2,12 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 class Clock {
+  public static final String ANSI_RED = "\u001B[31m";
+  public static final String ANSI_RESET = "\u001B[0m";
   public static void main(String[] args) {
     LocalDateTime dateTime = LocalDateTime.now();
     DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");
     String formattedTime = dateTime.format(dtf);
-    String redWord = "\u001B[31m" + "time" + "\u001B[37m";
-    System.out.printf("The current %s is %s \n", redWord, formattedTime);
+    System.out.printf("The current" + ANSI_RED + " time " + ANSI_RESET + "is %s \n", formattedTime);
   }  
 }
