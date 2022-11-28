5ab86560b82a8490bcaf3b5b796992949c820bce
ritaaktay <ritaaktay@gmail.com>
2022-11-28 12:33:15 +0000

    TDD basic Todo app

--- app/src/test/java/app/TodoTest.java
+++ app/src/test/java/app/TodoTest.java
@@ -1,23 +1,25 @@
 package app;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 
 import org.junit.Test;
 
 public class TodoTest {
-  @Test public void testHasEmptyTodoList() {
+  @Test 
+  public void testStartWithEmptyTodoList() {
     Todo todo = new Todo();
     ArrayList<String> empty = new ArrayList<String>();
-    assertEquals("initializes with empty todo list", empty, todo.getTodos());
+    assertTrue("initializes with empty todo list", todo.todos.isEmpty());
   }
 
-  @Test public void testAddsTodosToList() {
+  @Test 
+  public void testAddsTodosToList() {
     Todo todo = new Todo();
-    ArrayList<String> todos = new ArrayList<String>();
-    todos.add("Get milk");
+    ArrayList<String> expected = new ArrayList<String>();
+    expected.add("Get milk");
     todo.addTodo("Get milk");
-    assertEquals("adds todos to list", todos, todo.getTodos());
+    assertEquals("adds todos to list", expected, todo.getTodos());
   }
 }
