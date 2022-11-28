7ca764da4138e4ebd6ba0f151012d72274f5d165
ritaaktay <ritaaktay@gmail.com>
2022-11-28 07:50:11 +0000

    Gradle and JUnit

--- PriceDisplay.java
+++
@@ -1,7 +0,0 @@
-class PriceDisplay {
-  public static void main(String[] args) {
-    String item = "Coffee machine";
-    Double price = 15.99;
-    System.out.printf("The price for the item %s is %.2f\n", item, price);
-  }
-}
