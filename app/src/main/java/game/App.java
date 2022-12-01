d1c4efb2efd99982dd36942a65a2061a75ea1dcc
ritaaktay <ritaaktay@gmail.com>
2022-12-01 08:50:15 +0000

    Refactor App methods

--- app/src/main/java/game/App.java
+++ app/src/main/java/game/App.java
@@ -6,19 +6,39 @@ import java.util.Scanner;
 
 public class App {
   public static void main(String[] args) {
+    Game game = setup();
+    run(game);
+    end(game.win());
+  }
+
+  public static Game setup() {
+    print("Today your word to guess is:");
     ArrayList<String> dict = new ArrayList<String>(Arrays.asList("SAILING"));
     WordChooser wChooser = new WordChooser(dict);
-    Game game = new Game(wChooser);
-    System.out.println("Today your word to guess is:");
+    return new Game(wChooser);
+  }
+
+  public static void run(Game game){
     Scanner sc = new Scanner(System.in);
     while(game.getRemainingAttempts() > 0 && !game.win()) {
-      System.out.println(game.getWordToGuess());
+      print(game.getWordToGuess());
       if (!game.win()) {
-        System.out.printf("Guesses: %d\n", game.getRemainingAttempts());
         Character letter = sc.nextLine().charAt(0);
         game.guessLetter(letter);
       }
     }
     sc.close();
+  }
+
+  public static void end(Boolean win) {
+    if (win) {
+      print("CONGRATULATIONS!");
+    } else {
+      print("BETTER LUCK NEXT TIME...");
+    }
+  }
+
+  public static void print(String message) {
+    System.out.println(message);
   }
 }
